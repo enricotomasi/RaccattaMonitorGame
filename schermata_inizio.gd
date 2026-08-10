b79bfae57e43497f77966b33141e648ca4784492
Enrico Tomasi <enricotomasi@outlook.com>
2026-08-10 15:54:09 +0200

    update

--- schermata_inizio.gd
+++ schermata_inizio.gd
@@ -6,4 +6,6 @@ const SCENA_GIOCO = "res://game.tscn"
 func _input(event: InputEvent) -> void:
 	
 	if event.is_action_pressed("Click"):
+		Global.lives = Global.max_lives
+		Global.score = 0
 		get_tree().change_scene_to_file(SCENA_GIOCO)
